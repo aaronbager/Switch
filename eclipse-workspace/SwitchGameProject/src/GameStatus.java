c5f0ecbf20c51320c82fdfac0d72f0e573b81312
stewajos <35934674+stewajos@users.noreply.github.com>
2018-02-28 16:03:03 -0500

    added turns

--- eclipse-workspace/SwitchGameProject/src/GameStatus.java
+++ eclipse-workspace/SwitchGameProject/src/GameStatus.java
@@ -1,7 +1,9 @@
 
 public enum GameStatus {
 	 IN_PROGRESS, /* game is still in progress */
-	 USER_WON,    /* The game board is full and user has the most tiles*/
-	 USER_LOST    /* The game board is full and user does not have the most tiles */
+	 PlayerOneWins,    /* The game board is full and user has the most tiles*/
+	 PlayerTwoWins,    /* The game board is full and user does not have the most tiles */
+	 PlayerOneTurn,
+	 PlayerTwoTurn
 	}
 
