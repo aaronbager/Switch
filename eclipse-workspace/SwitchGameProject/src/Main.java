82e4b6984bd174459cf68113752941f25f7a1526
aaronbager <35773667+aaronbager@users.noreply.github.com>
2018-03-01 15:33:28 -0500

    Update Main.java

--- eclipse-workspace/SwitchGameProject/src/Main.java
+++ eclipse-workspace/SwitchGameProject/src/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.geometry.Insets;
+import javafx.scene.control.Label; 
 
 import java.util.Arrays;
 
@@ -18,25 +19,22 @@ import javafx.scene.paint.Color;
  
 public class Main extends Application{
 	
-	Cell[][] gameBoard = new Cell[8][8];
 	int game_board[][] = new int[8][8];
-	Button[][] gameBoardNodes = new Button[8][8];
-	//reversi rules = new reversi(); 
+	Button[][] gameBoardNodes = new Button[8][8]; 
 	Rules rules = new Rules(); 
 	GridPane board = new GridPane(); 
 	boolean isLegal = false; 
-	int value;
+	int blackScore = 0, whiteScore = 0;
+	Label whiteScoreLabel = new Label();
+	Label blackScoreLabel = new Label();
+	
 	//Image reversiSquare = new Image(getClass().getResourceAsStream("reversiSquare.jpg"), 50, 50, false, false);
 	public static void main(String args[]) {
 		
-		//Stage primaryStage = new Stage(); 
-		//start(primaryStage);
 		launch(args);
 	}
 	public void start(Stage primaryStage) {
 		
-		//rules.resizeBoard(8);
-//		game_board = rules.start(); //was throwing error, probably due to the fact array is not initialized yet
 		for(int i = 0; i < 8; i++) {
 			for(int j = 0; j < 8; j++) {
 				int column, row; 
@@ -47,13 +45,11 @@ public class Main extends Application{
 		//		gameBoardNodes[i][j].setGraphic(new ImageView(reversiSquare));
 				gameBoardNodes[i][j].setPrefHeight(64);
 				gameBoardNodes[i][j].setPrefWidth(64);
-				//gameBoard[i][j] = new Cell(i,j,0);
 				board.add(gameBoardNodes[i][j], j, i);
 				
 				gameBoardNodes[i][j].setOnAction(new EventHandler<ActionEvent>() {
 					@Override
 					public void handle(ActionEvent event) {
-						
 						if(rules.isMoveLegal(row, column))
 						{	
 							System.out.println(Arrays.deepToString(game_board).replace("], ", "]\n"));
@@ -61,20 +57,11 @@ public class Main extends Application{
 							game_board = rules.placeTile(row, column);
 							System.out.println(Arrays.deepToString(game_board).replace("], ", "]\n"));
 							System.out.println();
-				//			rules.legalMove(row, column); 
-			//				rules.placeTile(row, column);
-			//				gameBoard[row][column].setRow(row);
-					//		gameBoard[row][column].setColumn(column);
-						//	gameBoard[row][column].setValue(rules.passTurn()); 
-			//				board.add(new Circle(32, Color.WHITE), column, row);
-				//			gameBoard = rules.checkTiles(gameBoard[row][column]);
 							for(int k = 0; k < 8; k++)
 							{
 								int row1;
 								int column1; 
 								for(int l = 0; l < 8; l++) {
-			//						row1 = gameBoard[k][l].getRow();
-			//						column1 = gameBoard[k][l].getColumn();
 									if(game_board[k][l] == 1) {
 										board.add(new Circle(32, Color.BLACK), l, k);
 									}
@@ -87,21 +74,30 @@ public class Main extends Application{
 						else {
 							System.out.println("NOT A LEGAL MOVE!");
 						}
-						
-//						rules.legalMove(row, column); 
-//						rules.placeTile(row, column);
-//						 
-//						 board.add(new Circle(32, Color.WHITE), column, row);
+						blackScoreLabel.setText("Black Score:" + String.valueOf(rules.getScore(1)));
+						whiteScoreLabel.setText("White Score:" + String.valueOf(rules.getScore(2)));
+						int possibleMoves = 0; 
+						for(int i = 0; i < 7; i++) {
+							for(int j = 0; j < 7; j++) {
+								if(rules.isMoveLegal(i, j)) {
+									possibleMoves++; 
+								}
+								
+								if(possibleMoves == 0) {
+									int winner = rules.declareWinner(); 
+								}
+							}
+						}
 					}
 				});
 			}
 			
 		}
+		board.add(blackScoreLabel, 11, 11);
+		board.add(whiteScoreLabel, 11, 12);
+		blackScoreLabel.setText("Black Score: 2");
+		whiteScoreLabel.setText("White Score: 2");
 		game_board = rules.start(); 
-//		gameBoard[3][3].setValue(2);
-//		gameBoard[3][4].setValue(1);
-//		gameBoard[4][3].setValue(1);
-//		gameBoard[4][4].setValue(2);
 		board.add(new Circle(32, Color.WHITE), 3, 3);
 		board.add(new Circle(32, Color.BLACK), 3, 4);
 		board.add(new Circle(32, Color.BLACK), 4, 3);
@@ -111,6 +107,6 @@ public class Main extends Application{
 		primaryStage.setTitle("Othello");
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		//rules.start(); 
+
 	}
 }
