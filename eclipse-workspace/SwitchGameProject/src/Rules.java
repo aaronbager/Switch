86269fb4206de8adb73944313d60964327a04e4a
jessricks <ricksgej@mail.gvsu.edu>
2018-03-01 16:29:44 -0500

    Update Rules.java

--- eclipse-workspace/SwitchGameProject/src/Rules.java
+++ eclipse-workspace/SwitchGameProject/src/Rules.java
@@ -1,10 +1,10 @@
 import java.util.Arrays;
 
 public class Rules {
-	private int[][] board = new int[8][8]; //represents the board, 0 is empty, 1 is black, 2 is white
-	private int turn; //1 for black, 2 for white, 3 for black wins, 4 for white wins
+	int[][] board = new int[8][8]; //represents the board, 0 is empty, 1 is black, 2 is white
+	int turn; //1 for black, 2 for white, 3 for black wins, 4 for white wins
 	
-	public int[][] start(){ // initializes the rules class
+	int[][] start(){ // initializes the rules class
 		for(int i = 0; i < 8; i++) {
 			for(int j = 0; j < 8; j++) {
 				//set all the values in board to 0, showing all spaces are empty
@@ -19,26 +19,223 @@ public class Rules {
 		return board; 
 	}
 
-	public boolean isMoveLegal(int row, int column)
-	{
-		if(board[row][column] == 0)
-			return true; 
+	boolean isMoveLegal(int row, int column)
+	{	
+		if(board[row][column] == 0) {
+			
+			if(checkNeighbors(row,column)) {
+			
+				return true;
+			}
+		}
 		
 		return false; 
+		
 	}
-	public int[][] placeTile(int row, int column){
+	
+	boolean checkNeighbors(int row, int column) {
+		
+		int tilecount = 0;
+		
+		//check center pieces
+		if(row != 7 && row != 0 && column != 7 && column != 0) {
+			
+			//check right
+			if(board[row+1][column] != 0) {
+				
+				tilecount += 1;
+			}
+			//check top
+			if(board[row][column-1] != 0) {
+				
+				tilecount += 1;	
+			}
+			//check left
+			if(board[row-1][column] != 0) {
+				
+				tilecount += 1;
+			}
+			//check bottom
+			if(board[row][column+1] != 0) {
+				
+				tilecount += 1;	
+			}
+		}
+		
+		//check bottom
+		if(row == 7 && column != 0 && column != 7) {
+			
+			//check right
+			if(board[row][column+1] != 0) {
+				
+				tilecount += 1;
+			}
+			//check left
+			if(board[row-1][column] != 0) {
+				
+				tilecount += 1;
+			}
+			//check top
+			if(board[row][column-1] != 0) {
+				
+				tilecount += 1;	
+			}
+			
+		}
+		
+		//check top
+		if(row == 0 && column != 0 && column != 7) {
+			
+			//check right
+			if(board[row+1][column] != 0) {
+				
+				tilecount += 1;
+			}
+			//check bottom
+			if(board[row][column+1] != 0) {
+				
+				tilecount += 1;	
+			}
+			//check left
+			if(board[row][column-1] != 0) {
+				
+				tilecount += 1;
+			}
+		}
+		
+		//check left
+		if(column == 0 && row != 0 && row != 7) {
+			
+			//check right
+			if(board[row][column+1] != 0) {
+				
+				tilecount += 1;
+			}
+			//check top
+			if(board[row-1][column] != 0) {
+				
+				tilecount += 1;	
+			}
+			//check bottom
+			if(board[row+1][column] != 0) {
+				
+				tilecount += 1;	
+			
+		}
+		}
+			
+		//check right
+		if(column == 7 && row != 0 && row != 7) {
+			
+			//check top
+			if(board[row-1][column] != 0) {
+				
+				tilecount += 1;	
+			}
+			//check left
+			if(board[row][column-1] != 0) {
+				
+				tilecount += 1;
+			}
+			//check bottom
+			if(board[row+1][column] != 0) {
+				
+				tilecount += 1;	
+			}
+			
+		}
+		
+		//check top left
+		if(column == 0 && row == 0) {
+			
+			//check bottom
+			if(board[row+1][column] != 0) {
+				
+				tilecount += 1;	
+			}
+			//check right
+			if(board[row][column+1] != 0) {
+				
+				tilecount += 1;
+			}
+			
+		}
+		
+		//check top right
+		if(column == 7 && row == 0) {
+			
+			//check left
+			if(board[row][column-1] != 0) {
+				
+				tilecount += 1;
+			}
+			//check bottom
+			if(board[row+1][column] != 0) {
+				
+				tilecount += 1;	
+			}
+			
+		}
+		
+		//check bootom left
+		if(column == 0 && row == 7) {
+			
+			//check right
+			if(board[row][column+1] != 0) {
+				
+				tilecount += 1;
+			}
+			//check top
+			if(board[row-1][column] != 0) {
+				
+				tilecount += 1;	
+			}
+		}
+		
+		//check bottom right
+		if(column == 7 && row == 7) {
+			
+			//check top
+			if(board[row-1][column] != 0) {
+				
+				tilecount += 1;	
+			}
+			//check left
+			if(board[row][column-1] != 0) {
+				
+				tilecount += 1;
+			}
+			
+			
+		}	
+			
+		
+		
+		if(tilecount > 0) {
+			
+			return true;
+		}
+		
+		return false;
+		
+	}
+	
+	
+	
+	int[][] placeTile(int row, int column){
 		
 		
 		System.out.println(Arrays.deepToString(board).replace("], ", "]\n"));
 		board[row][column] = turn; 
 		int oppositeTurn = (turn == 1) ? 2 : 1;
 		System.out.println();
-		int tempColumn = column;
-		int tempRow = row; 
+
 		//check left
-	       OUTER:for(int i = column; i > 0; i-- ) { 
-	           if(board[row][i-1] == turn) {
-	               for(int j = i - 1; j < column; j++) {
+		int counter = 0; 
+	       OUTER:for(int i = column; i > 0; i-- ) {
+	    	   counter++; 
+	           if(board[row][i] == board[row][i-1]) {
+	               for(int j = i + 1; j > column; j--) {
 	                   board[row][j] = turn;
 	               }
 	               if(board[row][i] == 0) {
@@ -47,82 +244,43 @@ public class Rules {
 	                break;
 	           }
 	           
-	           if(board[row][i-1] == 0) {
+	           if(board[row][i] == 0) {
 	               break;
 	           }
 	       }
-	    //check right
-		OUTER:for(int i = column; i < 7; i++) {
-			if(board[row][i+1] == turn) {
-				for(int j = i + 1; j > column; j--) {
-					board[row][j] = turn; 
-				}
-				if(board[row][i+1] == 0) {
-					break;
-				}
-				break; 
-			}
-		}
-		//check up
-	       OUTER:for(int i = row; i > 0; i-- ) { 
-	           if(board[i-1][column] == turn) {
-	               for(int j = i - 1; j < row; j++) {
-	                   board[j][column] = turn;
-	               }
-	               if(board[i-1][column] == 0) {
-		               break OUTER;
-		           }
-	                break;
-	           }
-	           
-	           if(board[i][column] == 0) {
-	               break;
-	           }
-	       }
-		//check down
-		OUTER:for(int i = row; i < 7; i++) {
-			if(board[i+1][column] == turn) {
-				for(int j = i + 1; j > row; j--) {
+	       		//check right
+		
+		/*for(int i = row; i < 8; i++ ) {
+			System.out.println(Arrays.deepToString(board).replace("], ", "]\n"));
+			System.out.println();
+			if(board[i][column] == board[i + 1][column]) {
+				System.out.println(Arrays.deepToString(board).replace("], ", "]\n"));
+				System.out.println();
+				for(int j = i + 1; j < row; j--) {
+					System.out.println(Arrays.deepToString(board).replace("], ", "]\n"));
+					System.out.println();
 					board[j][column] = turn; 
 				}
-				if(board[i][column] == 0) {
-					break OUTER;
-				}
+			}*/
+	       
+	       /*for(int i = column + 1; i < 8; i++ ) {
+	           if(board[row][i] == board[row][i+1]) {
+	               for(int j = i + 1; j < column; j--) {
+	                   board[row][j] = turn;
+	               }
+	                break;
+	           }
+
+			if(board[i][column] == 0) {
 				break; 
 			}
 		}
+*/		//check up
+		//check down
 		//check diagonal up left
-	    for(int i = column; i > 0; i--) {
-	    	for(int j = row; j > 0; j--) {
-	    		if(board[j-1][i-1] == turn) {
-	    			board[j][i] = turn;
-	    		}
-	    	}
-	    }
 		//check diagonal down left
-	    for(int i = column; i > 0; i--) {
-	    	for(int j = row; j < 7; j++) {
-	    		if(board[j+1][i-1] == turn) {
-	    			board[j][i] = turn; 
-	    		}
-	    	}
-	    }
 		//check diagonal up right
-	    for(int i = column; i < 7; i++) {
-	    	for(int j = row; j < 7; j++) {
-	    		if(board[j+1][i+1] == turn) {
-	    			board[j][i] = turn; 
-	    		}
-	    	}
-	    }
 		//check diagonal down right
-	    for(int i = column; i < 7; i++) {
-	    	for(int j = row; j > 0; j--) {
-	    		if(board[j-1][i+1] == turn) {
-	    			board[j][i] = turn; 
-	    		}
-	    	}
-	    }
 		if(turn == 1) {
 			turn = 2;
 		}
@@ -131,41 +289,7 @@ public class Rules {
 		} //changes turn
 		return board;
 	}
-	public int declareWinner() {
-		int blackPieces = 0;
-		int whitePieces = 0; 
-		for(int i = 0; i < 7; i++) {			
-			for(int j = 0; j < 7; j++) {
-				if(board[i][j] == 1) {
-					blackPieces++;
-				}
-				else if(board[i][j] == 2) {
-					whitePieces++; 
-				}
-			}
-		}
-		if(blackPieces > whitePieces) {
-			return 1; 
-		}
-		else if(blackPieces < whitePieces) {
-			return 2; 
-		}
-		else {
-			return 3; 
-		}
-	}
-	public int getTurn() {
+	int getTurn() {
 		return turn; 
-	}
-	public int getScore(int color) {
-		int score = 0; 
-		for(int i = 0; i < 7; i++) {			
-			for(int j = 0; j < 7; j++) {
-				if(board[i][j] == color) {
-					score++;
-				}
-			}
-		}
-		return score; 
 	}
 }
