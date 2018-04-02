a24e6d2510210e5fb89c1bfe0e7b0c498f892d5c
aaronbager <35773667+aaronbager@users.noreply.github.com>
2018-04-02 15:26:39 -0400

    Delete GameStatus.java

--- eclipse-workspace/SwitchGameProject/src/GameStatus.java
+++
@@ -1,9 +0,0 @@
-
-public enum GameStatus {
-	 IN_PROGRESS, /* game is still in progress */
-	 PlayerOneWins,    /* The game board is full and user has the most tiles*/
-	 PlayerTwoWins,    /* The game board is full and user does not have the most tiles */
-	 PlayerOneTurn,
-	 PlayerTwoTurn
-	}
-
